09a1563877b1be6fc5d62cc96cd7831041ecd8cb
Nick Kokenis <37492342+nkokenis@users.noreply.github.com>
2021-03-20 16:48:57 -0700

    Update 3/20/21 #2

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-//Function definitions
-void assemble(char* listingFilePath);
+//Global variables
+int numLines = 0;
+
+//Function declarations
+struct LineData* assemble(char* listingFilePath);
 void parseInstruction(char* instruction);
+
+struct LineData
+{
+    char address[4];
+    char label[16];
+    char instruction[16];
+    char operand[64];
+    char objcode[8];
+    int lineNumber;
+};
+
 
 int main(int argc, char** argv)
 {
@@ -16,13 +31,15 @@ int main(int argc, char** argv)
 
     for(int i = 1; i < argc; i++)
     {
-        assemble(argv[i]);
+        struct LineData* data = assemble(argv[i]);
+        //Call other functions after assembling
+        free(data);
     }
 
     return 0;
 }
 
-void assemble(char* listingFilePath)
+struct LineData* assemble(char* listingFilePath)
 {
     //Create a FILE pointer using the file path provided
     FILE* listingFile = fopen(listingFilePath,"r");
@@ -30,20 +47,27 @@ void assemble(char* listingFilePath)
     {
         //The file does not exist or couldn't be found
         printf("File not found.");
+        return NULL;
     }
-
-    char address[4] = {};
-    char label[16] = {};
-    char instruction[16] = {};
-    char operand[64] = {};
-    char objcode[8] = {};
-
-    int numLines = 0;
 
     int index = 0;
     int len = 0;
+    int lineNum = 0;
 
-    int newWhiteSpace = 0;
+    while(1)
+    {
+        char ch = fgetc(listingFile);
+        if(feof(listingFile))
+        {
+            break;
+        }
+        else
+        {
+            numLines++;
+        }
+    }
+    struct LineData* data = malloc(sizeof(struct LineData) * numLines);
+    rewind(listingFile);
 
     //Iterate through the file, character by character
     while(1)
@@ -58,56 +82,49 @@ void assemble(char* listingFilePath)
 
         if(ch == ' ')
         {
-            if(newWhiteSpace == 0)
-            {
-                printf("%d",index);
-                len = 0;
-                index++;
-                newWhiteSpace = 1;
-            }
+            printf("%d",index);
+            len = 0;
+            index++;
+            while(fgetc(listingFile) == ' ');
         }
         else if(ch == '\n')
         {
             printf("%d\n",index);
-            numLines++;
+            data[lineNum].lineNumber = lineNum;
+            lineNum++;
             len = 0;
             index = 0;
-            newWhiteSpace = 0;
         }
         else
         {
             if(index == 0)
             {
-                address[len] = ch;
-                newWhiteSpace = 0;
+                data[lineNum].address[len] = ch;
                 len++;
             }
             else if(index == 1)
             {
-                label[len] = ch;
-                newWhiteSpace = 0;
+                data[lineNum].label[len] = ch;
                 len++;
             }
             else if(index == 2)
             {
-                instruction[len] = ch;
-                newWhiteSpace = 0;
+                data[lineNum].instruction[len] = ch;
                 len++;
             }
             else if(index == 3)
             {
-                operand[len] = ch;
-                newWhiteSpace = 0;
+                data[lineNum].operand[len] = ch;
                 len++;
             }
             else if(index == 4)
             {
-                objcode[len] = ch;
-                newWhiteSpace = 0;
+                data[lineNum].objcode[len] = ch;
                 len++;
             }
         }
     }
+    return data;
 }
 
 void parseInstruction(char* instruction)
